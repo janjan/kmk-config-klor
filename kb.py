db44916a162c2d38538be09d9dd05e3d3bb0becf
Your Name <70295002+moritz-john@users.noreply.github.com>
2022-12-31 22:37:05 +0100

    Fixes

--- kb.py
+++ kb.py
@@ -38,6 +38,7 @@ class KMKKeyboard(_KMKKeyboard):
     tx = pins[1]
     buzzer_a = pins[11]
     rgb_pixel_pin = pins[0]
+    
     # FOR POLYDACTYL
     # led_key_pos =[
     #        18, 13, 12,  6,  5,                  26, 27, 33, 34, 39,
@@ -45,6 +46,9 @@ class KMKKeyboard(_KMKKeyboard):
     #    20, 16, 15, 10,  8,  3,                  24, 29, 31, 36, 37, 41,
     #                     9,  2, 1, 0,    21, 22, 23, 30,
     # ]
+    # brightness_limit = 0.3
+    # num_pixels = 42
+
     # FOR KONRAD
     led_key_pos =[
            18, 13, 12,  6,  5,                  26, 27, 33, 34, 39,
@@ -53,7 +57,7 @@ class KMKKeyboard(_KMKKeyboard):
                         9,  2, 1,           22, 23, 30,
     ]
     brightness_limit = 0.3
-    num_pixels = 42
+    num_pixels = 40
     # NOQA
     # flake8: noqa
     coord_mapping = [
